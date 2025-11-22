3fe5bf630d6bbfde014b332fa0e2074ee36b31da
rendyt <raihanadityap01@gmail.com>
2025-11-22 09:22:36 +0700

    update

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 'use client';
-import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import TentangKami from './components/TentangKami';
